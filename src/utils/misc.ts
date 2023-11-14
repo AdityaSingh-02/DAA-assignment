a8b831410255cbe66db1c689290d56d5b4a3a252
AdityaSingh-02 <asaditya2002@gmail.com>
2023-11-14 19:27:33 +0530

    add: graph 2d

---
+++ src/utils/misc.ts
@@ -0,0 +1,13 @@
+export const data = {
+    "nodes": [
+      {"id": "Myriel", "group": 1},
+      {"id": "Napoleon", "group": 1},
+      {"id": "Mlle.Baptistine", "group": 1},
+      {"id": "Mme.Magloire", "groupt": 1},
+    ],
+    "links": [
+      {"source": "Napoleon", "target": "Myriel", "value": 1},
+      {"source": "Mlle.Baptistine", "target": "Myriel", "value": 8},
+      {"source": "Mme.Magloire", "target": "Mlle.Baptistine", "value": 6},
+    ]
+  }
