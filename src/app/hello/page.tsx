6309d45d2762f0fc1234a8775fae2ce8c690bb36
AdityaSingh-02 <asaditya2002@gmail.com>
2023-11-15 16:13:08 +0530

    chore: graph data

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -9,35 +9,17 @@ const Hello = () => {
       <ForceGraph2D
         graphData={data}
         backgroundColor="lightGray"
-        nodeAutoColorBy="group"
         nodeCanvasObject={(node, ctx, globalScale) => {
           const label = node.id;
           const fontSize = 12 / globalScale;
-          ctx.font = `${fontSize}px Sans-Serif`;
-          const textWidth = ctx.measureText(label).width;
-          const bckgDimensions = [textWidth, fontSize].map(
-            (n) => n + fontSize * 0.2
-          ); // some padding
-
-          ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-
+          ctx.font = `${fontSize}px sans-serif`;
           ctx.textAlign = "center";
-          ctx.textBaseline = "middle";
-          ctx.fillStyle = node.color;
+          ctx.fillStyle = "green";
           ctx.fillText(label, node.x!, node.y!);
-
-          node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
         }}
-        nodePointerAreaPaint={(node, color, ctx) => {
-          ctx.fillStyle = color;
-          const bckgDimensions = node.__bckgDimensions;
-          bckgDimensions &&
-            ctx.fillRect(
-              node.x! - bckgDimensions[0] / 2,
-              node.y! - bckgDimensions[1] / 2,
-              ...bckgDimensions
-            );
-        }}
+        linkDirectionalArrowLength={5}
+        linkWidth={2}
+        linkAutoColorBy={"source"}
       />
       ,
     </>
