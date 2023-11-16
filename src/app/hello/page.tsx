33c696c6902b93bbcfb065af83b968a16c7bc36e
AdityaSingh-02 <asaditya2002@gmail.com>
2023-11-16 11:25:36 +0530

    fix: graph values

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,20 +1,30 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ForceGraph2D from "react-force-graph-2d";
 import { data } from "@/utils/misc";
+import { IGraphData } from "@/types/IGraphData";
 
 const Hello = () => {
+  const [linkData, setLinkData] = useState<IGraphData>();
+
+  useEffect(() => {
+    setLinkData(data);
+  }, [data]);
+
   return (
     <>
       <ForceGraph2D
         graphData={data}
         backgroundColor="lightGray"
+        nodeCanvasObjectMode={() => "after"}
+        onNodeClick={(node) => console.log(node.id)}
         nodeCanvasObject={(node, ctx, globalScale) => {
           const label = node.id;
           const fontSize = 12 / globalScale;
           ctx.font = `${fontSize}px sans-serif`;
           ctx.textAlign = "center";
           ctx.fillStyle = "Red";
+          ctx.textBaseline = "middle";
           ctx.fillText(label, node.x!, node.y!);
         }}
         linkDirectionalArrowLength={5}
